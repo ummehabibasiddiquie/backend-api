71dbbe99394d98e5d0fcf29f36ebfcff4cef5d27
ummehabibasiddiquie <ummehabiba.siddiquie@transformsolution.net>
2026-03-06 13:37:36 +0530

    fix: Update test report date in fetch_data() to 2026-02-27

--- billable_report_autosend.py
+++ billable_report_autosend.py
@@ -63,7 +63,7 @@ def fetch_data():
         # report_date = today - timedelta(days=1)
 
         # TEST DATE
-        report_date = datetime.strptime("2026-02-26", "%Y-%m-%d").date()
+        report_date = datetime.strptime("2026-02-27", "%Y-%m-%d").date()
         report_month = report_date.strftime("%b%Y").upper()
 
         logging.info(f"Fetching data for report date {report_date}")
